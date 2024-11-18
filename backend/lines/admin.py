b48a8aa7b3945f60b9a09fd77ea9016797c69d08
DorofeevAlexandr <amd_86@mail.ru>
2024-11-18 11:08:13 +0300

    Отображение данных в админке

--- backend/lines/admin.py
+++ backend/lines/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Counters, Lines, LinesCurrentParams
 
-# Register your models here.
+@admin.register(Counters)
+class CountersAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Lines)
+class LinesAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(LinesCurrentParams)
+class LinesCurrentParamsAdmin(admin.ModelAdmin):
+    pass
--- backend/lines/models.py
+++ backend/lines/models.py
@@ -1,7 +1,9 @@
+import uuid
 from django.db import models
 
 
 class Counters(models.Model):
+    id = models.IntegerField(primary_key=True, editable=False)
     time = models.DateTimeField()
     lengths = models.TextField(blank=True, null=True)  # This field type is a guess.
     connection_counters = models.TextField(blank=True, null=True)  # This field type is a guess.
@@ -9,10 +11,16 @@ class Counters(models.Model):
     class Meta:
         managed = False
         db_table = 'counters'
+        verbose_name = 'Счетчик'
+        verbose_name_plural = 'Счетчики'
+
+    def __str__(self):
+        return str(self.time)
 
 
 
 class Lines(models.Model):
+    id = models.IntegerField(primary_key=True, editable=False)
     line_number = models.IntegerField(unique=True)
     name = models.CharField(unique=True, max_length=1)
     pseudonym = models.CharField(unique=True, max_length=1)
@@ -29,10 +37,15 @@ class Lines(models.Model):
     class Meta:
         managed = False
         db_table = 'lines'
+        verbose_name = 'Линия'
+        verbose_name_plural = 'Линии'
+
+    def __str__(self):
+        return f'{self.id} - {self.name} - {self.pseudonym}'
 
 
 class LinesCurrentParams(models.Model):
-    id = models.ForeignKey(Lines, models.DO_NOTHING, db_column='id', blank=True, null=True)
+    id = models.IntegerField(primary_key=True, editable=False)
     no_connection_counter = models.BooleanField(blank=True, null=True)
     indicator_value = models.IntegerField(blank=True, null=True)
     length = models.FloatField(blank=True, null=True)
@@ -42,5 +55,8 @@ class LinesCurrentParams(models.Model):
     class Meta:
         managed = False
         db_table = 'lines_current_params'
+        verbose_name = 'Текущий параметр'
+        verbose_name_plural = 'Текущие параметры'
 
-
+    def __str__(self):
+        return f'{self.id}'
