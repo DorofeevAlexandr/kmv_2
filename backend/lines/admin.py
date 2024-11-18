53e4409de39cbd7ba682b142163698e55840407d
DorofeevAlexandr <amd_86@mail.ru>
2024-11-18 11:31:50 +0300

    Добавил фильтрацию и поиск параметров в админке

--- backend/lines/admin.py
+++ backend/lines/admin.py
@@ -3,12 +3,27 @@ from .models import Counters, Lines, LinesCurrentParams
 
 @admin.register(Counters)
 class CountersAdmin(admin.ModelAdmin):
-    pass
+    # Отображение полей в списке
+    list_display = ('time', )
+    # Фильтрация в списке
+    list_filter = ('time',)
+    # Поиск по полям
+    search_fields = ('time',)
 
 @admin.register(Lines)
 class LinesAdmin(admin.ModelAdmin):
-    pass
+    # Отображение полей в списке
+    list_display = ('line_number', 'name', 'pseudonym', 'k')
+    # Фильтрация в списке
+    list_filter = ('line_number', 'pseudonym',)
+    # Поиск по полям
+    search_fields = ('line_number', 'pseudonym',)
 
 @admin.register(LinesCurrentParams)
 class LinesCurrentParamsAdmin(admin.ModelAdmin):
-    pass
+    # Отображение полей в списке
+    list_display = ('id', 'length', 'speed_line',)
+    # Фильтрация в списке
+    list_filter = ('id',)
+    # Поиск по полям
+    search_fields = ('id',)
