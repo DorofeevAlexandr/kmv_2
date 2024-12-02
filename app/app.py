b5b75f7f56c2daa206f7e34b4a40fabcbe2268bc
DorofeevAlexandr <amd_86@mail.ru>
2024-12-02 12:02:16 +0300

    Перенес app.py в докер, убрал принты

--- app/app.py
+++ app/app.py
@@ -17,13 +17,13 @@ def postgres_engine():
     db_name = os.environ.get('DB_NAME')
     db_user = os.environ.get('DB_USER')
     db_pass = os.environ.get('DB_PASSWORD')
-    # db_host = os.environ.get('DB_HOST', 'db')
-    db_host = 'localhost'
+    db_host = os.environ.get('DB_HOST', 'db')
+    # db_host = 'localhost'
     db_port = str(os.environ.get('DB_PORT', 5432))
 
     # Connecto to the database
     db_string = f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
-    print(db_string)
+    # print(db_string)
     result_engine = create_engine(db_string)
     print(result_engine)
     return result_engine
@@ -34,7 +34,7 @@ def read_counters_save_current_params(p_engine):
         registers = control_plc.get_registers()
         control_plc.write_register(reg_adr=1, new_value=123)
         lines_params = read_lines_params_in_base(session=db)
-        print(lines_params)
+        # print(lines_params)
         read_plc_counters(db, lines_params, registers)
         read_serial_port_counters(db, lines_params)
         try:
@@ -45,11 +45,11 @@ def read_counters_save_current_params(p_engine):
 def read_current_params_save_in_base(p_engine):
     with Session(autoflush=False, bind=p_engine) as db:
         current_lengths = read_current_lengths_in_base(db)
-        print(current_lengths)
+        # print(current_lengths)
         current_connections = read_current_connections_in_base(db)
-        print(current_connections)
+        # print(current_connections)
         lines_name = read_lines_name(db)
-        print(lines_name)
+        # print(lines_name)
 
         append_in_csv(lines_name=lines_name, current_lengths=current_lengths)
         add_in_base_counters(db, current_lengths=current_lengths, current_connections=current_connections)
@@ -67,10 +67,10 @@ if __name__ == '__main__':
 
         if dt.datetime.now() - dt_last_save_lengths >= dt.timedelta(seconds=60):
             dt_last_save_lengths = dt.datetime.now()
-            print('dt_last_save_lengths', dt_last_save_lengths)
+            # print('dt_last_save_lengths', dt_last_save_lengths)
             read_current_params_save_in_base(p_engine=engine)
 
         if dt.datetime.now() - dt_last_save_current_params >= dt.timedelta(seconds=12):
             read_counters_save_current_params(p_engine=engine)
             dt_last_save_current_params = dt.datetime.now()
-            print('read_counters_save_current_params', dt_last_save_current_params)
+            # print('read_counters_save_current_params', dt_last_save_current_params)
