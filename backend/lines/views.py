44cf62b9eb9d414fa3f56d0ae9b22e61405e383e
DorofeevAlexandr <amd_86@mail.ru>
2024-12-03 20:13:19 +0300

    Добавил пустые вьюхи

--- backend/lines/views.py
+++ backend/lines/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.template.loader import render_to_string
 
-# Create your views here.
+def index(request): # HttpRequest
+    # t = render_to_string('women/index.html')
+    # return HttpResponse(t)
+    return render(request, 'lines/index.html')
+
+
+def about(request):
+    return render(request, 'lines/about.html')
+
+
+def categories(request, cat_id):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
+
+
+def categories_by_slug(request, cat_slug):
+    if request.POST:
+        print(request.POST)
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
+
+
+def archive(request, year):
+    if year > 2023:
+        uri = reverse('cats', args=('sport', ))
+        return HttpResponsePermanentRedirect(uri)
+
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
+
