01622cd9f056f63caeabf969b8a4003ec7bf190d
DorofeevAlexandr <amd_86@mail.ru>
2024-12-04 03:37:21 +0300

    Получение списка длин

--- backend/lines/forms.py
+++ backend/lines/forms.py
@@ -3,9 +3,32 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.utils.deconstruct import deconstructible
+from django.db import models
 
 from .models import Counters
 
 class ReadDataCounters(forms.Form):
     day = forms.DateField(initial=dt.date.today)
     # department = forms.CharField(max_length=255)
+
+
+def get_counters_values_from_base(date: dt.date):
+    dtime_8_hours = dt.timedelta(hours=8)
+    dtime_1_days = dt.timedelta(days=1)
+    time_start = dt.datetime(year=date.year,
+                             month=date.month,
+                             day=date.day,
+                             hour=0,
+                             minute=0,
+                             second=0) + dtime_8_hours
+    time_end = time_start + dtime_1_days
+    counters = Counters.objects.filter(time__range=[time_start, time_end]).order_by('-time')
+
+    length_in_minute = [[] for _ in range(1440)]
+
+    for c in counters:
+        minutes = (c.time - time_start).seconds // 60
+        length_in_minute[minutes] = c.lengths
+    return  length_in_minute
+
+
--- backend/lines/views.py
+++ backend/lines/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpRespons
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.template.loader import render_to_string
-from .forms import ReadDataCounters
+from .forms import ReadDataCounters, get_counters_values_from_base
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Цех №1", 'url_name': 'index'},
@@ -15,7 +15,11 @@ def index(request):
     if request.method == 'POST':
         form = ReadDataCounters(request.POST, request.FILES)
         if form.is_valid():
-            print(form.cleaned_data)
+            select_date = form.cleaned_data.get('day', None)
+            print(select_date)
+            if select_date:
+                counters_values = get_counters_values_from_base(select_date)
+
     else:
         form = ReadDataCounters()
 
@@ -25,6 +29,24 @@ def index(request):
         'form': form,
     }
     return render(request, 'lines/index.html', context=data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def about(request):
