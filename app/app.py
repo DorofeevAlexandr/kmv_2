705a3bbf8fd6603686cff0ecddfe271b9260041c
DorofeevAlexandr <amd_86@mail.ru>
2025-11-18 11:17:00 +0300

    Добавил новый поток для записи данных в базу

--- app/app.py
+++ app/app.py
@@ -6,10 +6,8 @@ from sqlalchemy.orm import Session
 import time
 
 import control_plc
-from save_csv_file import append_in_csv
-from wear_lines import (read_lines_params_in_base, read_plc_counters, read_serial_port_counters,
-                        read_current_lengths_in_base, read_current_connections_in_base, read_lines_name,
-                        add_in_base_counters)
+
+from wear_lines import (read_lines_params_in_base, read_plc_counters, read_serial_port_counters)
 
 
 def postgres_engine():
@@ -41,21 +39,7 @@ def read_counters_save_current_params(p_engine):
         except Exception as e:
             print('error', e)
 
-def read_current_params_save_in_base(p_engine):
-    with Session(autoflush=False, bind=p_engine) as db:
-        current_lengths = read_current_lengths_in_base(db)
-        # print(current_lengths)
-        current_connections = read_current_connections_in_base(db)
-        # print(current_connections)
-        lines_name = read_lines_name(db)
-        # print(lines_name)
 
-        append_in_csv(lines_name=lines_name, current_lengths=current_lengths)
-        add_in_base_counters(db, current_lengths=current_lengths, current_connections=current_connections)
-        try:
-            pass
-        except Exception as e:
-            print('error', e)
 
 if __name__ == '__main__':
     engine = postgres_engine()
@@ -63,12 +47,6 @@ if __name__ == '__main__':
     dt_last_save_lengths = dt.datetime.now()
     while True:
         time.sleep(1)
-
-        if ((0 < dt.datetime.now().second < 30 ) and
-                (dt.datetime.now() - dt_last_save_lengths > dt.timedelta(seconds=30))):
-            dt_last_save_lengths = dt.datetime.now()
-            # print('dt_last_save_lengths', dt_last_save_lengths)
-            read_current_params_save_in_base(p_engine=engine)
 
         if dt.datetime.now() - dt_last_save_current_params >= dt.timedelta(seconds=30):
             read_counters_save_current_params(p_engine=engine)
--- app/control_plc.py
+++ app/control_plc.py
@@ -1,9 +1,15 @@
+import datetime as dt
 from dotenv import load_dotenv
 import os
 from pyModbusTCP.client import ModbusClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 import time
 from threading import Thread, Lock
 
+from save_csv_file import append_in_csv
+from wear_lines import (read_current_lengths_in_base, read_current_connections_in_base, read_lines_name,
+                        add_in_base_counters)
 
 load_dotenv()
 PLC_HOST = os.environ.get('PLC_HOST')
@@ -74,11 +80,60 @@ def write_register(reg_adr: int, new_value: int):
     stack_of_writable_register.append((reg_adr, new_value))
 
 
+def postgres_engine():
+    load_dotenv()
+    db_name = os.environ.get('DB_NAME')
+    db_user = os.environ.get('DB_USER')
+    db_pass = os.environ.get('DB_PASSWORD')
+    db_host = os.environ.get('DB_HOST', 'db')
+    # db_host = 'localhost'
+    db_port = str(os.environ.get('DB_PORT', 5432))
+
+    # Connecto to the database
+    db_string = f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
+    # print(db_string)
+    result_engine = create_engine(db_string)
+    print(result_engine)
+    return result_engine
+
+
+def read_current_params_save_in_base(p_engine):
+    with Session(autoflush=False, bind=p_engine) as db:
+        current_lengths = read_current_lengths_in_base(db)
+        # print(current_lengths)
+        current_connections = read_current_connections_in_base(db)
+        # print(current_connections)
+        lines_name = read_lines_name(db)
+        # print(lines_name)
+
+        append_in_csv(lines_name=lines_name, current_lengths=current_lengths)
+        add_in_base_counters(db, current_lengths=current_lengths, current_connections=current_connections)
+        try:
+            pass
+        except Exception as e:
+            print('error', e)
+
+def read_current_params_save_in_base_thread():
+    engine = postgres_engine()
+    dt_last_save_lengths = dt.datetime.now()
+    while True:
+        time.sleep(1)
+
+        if ((0 < dt.datetime.now().second < 30 ) and
+                (dt.datetime.now() - dt_last_save_lengths > dt.timedelta(seconds=30))):
+            print(dt_last_save_lengths)
+            dt_last_save_lengths = dt.datetime.now()
+            # print('dt_last_save_lengths', dt_last_save_lengths)
+            read_current_params_save_in_base(p_engine=engine)
+
 # start polling thread
 tp = Thread(target=polling_thread)
+read_save_in_base_thread = Thread(target=read_current_params_save_in_base_thread)
 # set daemon: polling thread will exit if main thread exit
 tp.daemon = True
+read_save_in_base_thread.daemon = True
 tp.start()
+read_save_in_base_thread.start()
 
 
 if __name__ == '__main__':
